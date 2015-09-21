153a8fc22bd4b9a31cd5f33d30e33443c18e72ec
May Dekkers <dekkers.may@gmail.com>
2015-09-22 00:10:42 +0300

    ex 16

---
+++ ex.16/ex.16/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("please enter your age - ");
+            int age = int.Parse(Console.ReadLine()); /*age, enterd by the user*/
+
+            int days = age * 365; /*clculate the days*/
+            int hours = days * 24; /*clculate the hours*/
+            int seconds = hours * 3600; /*clculate the seconds*/
+
+            Console.WriteLine("you have lived:");
+            Console.WriteLine(days+" days,");
+            Console.WriteLine(hours + " hours");
+            Console.WriteLine("and "+seconds + " seconds");
+        }
+    }
+}
--- ex14/ex14/Program.cs
+++ ex14/ex14/Program.cs
@@ -11,10 +11,10 @@ namespace ex14
         static void Main(string[] args)
         {
             Console.Write("please type degrees in farenhiet - ");
-            float farenhiet = float.Parse(Console.ReadLine());/*degrees in farenhiet enterd by the user*/
+            float farenhiet = float.Parse(Console.ReadLine()); /*degrees in farenhiet enterd by the user*/
 
             float celcius = (5 * (farenhiet - 32)) / 9;
-
+            Console.WriteLine();
             Console.WriteLine("{0} degrees in farenhiet is {1} degrees in celcius" ,farenhiet, celcius);
 
         }
