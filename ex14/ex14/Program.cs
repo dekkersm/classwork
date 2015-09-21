3464e4d9e84612b388623511f93b1c0f375efefb
May Dekkers <dekkers.may@gmail.com>
2015-09-21 22:46:14 +0300

    done exercize

--- ex14/ex14/Program.cs
+++ ex14/ex14/Program.cs
@@ -10,6 +10,13 @@ namespace ex14
     {
         static void Main(string[] args)
         {
+            Console.Write("please type degrees in farenhiet - ");
+            float farenhiet = float.Parse(Console.ReadLine());/*degrees in farenhiet enterd by the user*/
+
+            float celcius = (5 * (farenhiet - 32)) / 9;
+
+            Console.WriteLine("{0} degrees in farenhiet is {1} degrees in celcius" ,farenhiet, celcius);
+
         }
     }
 }
