55dc154f99817dea4170a5e1c7da0c324916a464
May Dekkers <dekkers.may@gmail.com>
2015-11-06 18:02:22 +0200

    ex 26

---
+++ ConsoleApplication3/ConsoleApplication3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            int num1divnum2, num1modnum2;
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            num1divnum2 = num1 / num2;
+            num1modnum2 = num1 % num2;
+            Console.WriteLine(num1divnum2);
+            Console.WriteLine(num1modnum2);
+
+        }
+    }
+}
---
+++ ex26/ex26/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();//creating the object 'randome'
+            int num1 = random.Next(1, 21);//raffling the first edge
+            int num2 = random.Next(1, 21);//raffling the second edge
+            int num3 = random.Next(1, 21);//raffling the third edge
+
+            if ((num1 + num2 > num3) && (num3 + num1 > num2) && (num2 + num3 > num1))//cheks if a triangle can be formed
+            {
+                int circ = num1 + num2 + num3;//calculate the circumference
+                double p = circ / 2;
+                double S = Math.Sqrt(p * (p - num1) * (p - num2) * (p - num3));//calculate the surface
+                Console.WriteLine("the raffled edges are: " + num1 + ", " + num2 + ", and " + num3);
+                Console.WriteLine("the circumference of the triangle is: " + circ + ", and the surface is: " + S);
+            }
+            else
+            {
+                Console.WriteLine("the raffled edges are: " + num1 + ", " + num2 + " and " + num3);
+                Console.WriteLine("you can't form a triangle");
+            }
+        }
+    }
+}
